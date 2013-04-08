a1d4be24cc9b24aabae60dbb0654787946873399
Travis Brown <tmbrown6@gmail.com>
2013-04-07 22:49:18 -0400

    Basing FramsiePl

---
+++ Library/FramsiePl.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+
+## Use strict syntax
+use strict;
+
+## Allow data dumping
+use Data::Dumper;
+
+## Define the package name
+package FramsiePl;
+
+## Create an instance placeholder
+my($oInstance);
+
+
+###
+## Define the constructor
+###
+sub new {
+
+}
+
+## Terminate
+1;
+
+## We're done
+__END__;
